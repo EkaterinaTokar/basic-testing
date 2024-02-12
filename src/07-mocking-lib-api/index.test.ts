65c2a960c80414154eeae1b290758ca50ca5780b
EkaterinaTokar <ekaterinatokar19@gmail.com>
2024-02-12 21:18:57 +0500

    feat: implemented snapshot testing

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,30 @@
 // Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+//import axios from 'axios';
+//import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios');
 
 describe('throttledGetDataFromApi', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+  });
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    /*const createSpy = jest.spyOn(axios, 'create');
+    await throttledGetDataFromApi('/todos');
+    expect(createSpy).toHaveBeenCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });*/
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    //
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    //
   });
 });
